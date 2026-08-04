ec59bc2f0500b122bcbe9918a177dabb4b47761a
bizharrasheed <knewpubg@gmail.com>
2026-08-05 00:41:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "social-hub-rain-v5";
+const CACHE_NAME = "social-hub-rain-v7"; // گوهارتنا ڤێرژنی بۆ پاقژکرنا داتایێن کەڤن
 const assets = [
   "./",
   "./index.html",
@@ -7,18 +7,41 @@ const assets = [
   "./manifest.json"
 ];
 
+// ئینستالکرنا کاشێ نووی
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
-    })
+    }).then(() => self.skipWaiting()) // زۆرەملێ کارپێکرنا ڤێرژنێ نووی
   );
 });
 
+// ڕەشکرنا کاشێ کەڤن هەر دەمێ ئەپلیکەیشن ڤەبوو
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// ستراتیژیا (Network First): دەستپێکێ ل ئینتەرنێتێ دگەڕیت، ئەگەر نەبوو پاشی دچیتە سەر کاشێ
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
+    fetch(fetchEvent.request)
+      .then(networkResponse => {
+        // ئەگەر ئینتەرنێت هەبوو، دیزاینێ نووی سەیڤ بکە د کاشی دا و پیشان بدە
+        const responseClone = networkResponse.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(fetchEvent.request, responseClone);
+        });
+        return networkResponse;
+      })
+      .catch(() => {
+        // ئەگەر ئینتەرنێت نەبوو، دیزاینا کەڤن یا سەیڤکری پیشان بدە
+        return caches.match(fetchEvent.request);
+      })
   );
 });
